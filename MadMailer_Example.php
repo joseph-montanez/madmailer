9f53da74e523d430f426710adf5b5fd706e882be
Nicholas Young <nicholaswyoung@Nicholas-Youngs-MacBook-Pro.local>
2009-12-27 09:17:57 -0600

    A few more lines of documentation

--- MadMailer_Example.php
+++ MadMailer_Example.php
@@ -1,6 +1,10 @@
 <?php
 require('MadMailer.class.php');
-$mailer = new MadMailer('YOUR USERNAME (OR E-MAIL ADDRESS)', 'YOUR API KEY');
+
+// There are a total of four arguments that can be used on the next line. The first two are shown here, the second two
+// are optional. The first of them is a debugger, which defaults to false, and the second, allows you to print
+// the transaction ID when sending a message. It also defaults to false.
+$mailer = new MadMailer('YOUR USERNAME (OR E-MAIL ADDRESS)', 'YOUR API KEY'); 
 
 // Get all lists for this account..
 $lists = $mailer->Lists();
